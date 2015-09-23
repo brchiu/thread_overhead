1783bf9c1452ee36eca15facc2cb11eadab11508
Bi-Ruei, Chiu <brchiu@ms35.hinet.net>
2015-09-23 21:06:26 +0800

    Adding total time calculation

--- thread_test.c
+++ thread_test.c
@@ -15,17 +15,20 @@ void *thread_f(void *user)
 int main(int argc, char *argv[])
 {
    pthread_t id;
+   unsigned int time_creation, time_termination;
 
    gettimeofday(&tv1, NULL);
    pthread_create(&id, NULL, thread_f, &time);
    pthread_join(id, NULL);
    gettimeofday(&tv3, NULL);
 
-   printf("thread create %ld us termination %ld\n", 
-          (tv2.tv_sec - tv1.tv_sec)*1000000UL+
-          (tv2.tv_usec - tv1.tv_usec),
-          (tv3.tv_sec - tv2.tv_sec)*1000000UL+
-          (tv3.tv_usec - tv2.tv_usec));
+   time_creation = (tv2.tv_sec - tv1.tv_sec) * 1000000UL +
+                   (tv2.tv_usec - tv1.tv_usec);
+   time_termination = (tv3.tv_sec - tv2.tv_sec) * 1000000UL +
+                      (tv3.tv_usec - tv2.tv_usec);
+
+   printf("thread create %ld us termination %ld total %ld\n", 
+          time_creation, time_termination, time_creation + time_termination);
 
    return 0;
 }
